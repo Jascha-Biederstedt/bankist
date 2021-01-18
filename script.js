bc225ecfdfb31f07619f61908eef8f28fdcecd6d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-01-18 11:10:16 +0100

    Implement deleting account

--- script.js
+++ script.js
@@ -167,6 +167,28 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    labelWelcome.textContent = 'Login to get started';
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
